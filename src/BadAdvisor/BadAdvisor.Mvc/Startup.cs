8063ea4b2491fcf09f4240e925e85c97a4ec35fd
Artem Mikulich <artemmikulich@coherentsolutions.com>
2023-04-25 19:43:14 +0300

    Deploy pipe

--- src/BadAdvisor/BadAdvisor.Mvc/Startup.cs
+++ src/BadAdvisor/BadAdvisor.Mvc/Startup.cs
@@ -27,7 +27,7 @@ namespace BadAdvisor.Mvc
             {
                 var tableClient = new TableClient(
                     new Uri("http://127.0.0.1:10002/devstoreaccount1"),
-                    "badadvisor",
+                    "commitmessage",
                     new TableSharedKeyCredential("devstoreaccount1",
                         "Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw=="));
 
